211c7b25a9c5ad585d5053986be3997f955e65f2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-29 17:05:53 +0530

    feat: map local cartItems data to initialState

--- src/features/cart/cartSlice.tsx
+++ src/features/cart/cartSlice.tsx
@@ -1,18 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { cartItems, CartItem } from '../../cartItems'
 
 /**
  * @see https://redux-toolkit.js.org/usage/usage-with-typescript#createslice
  */
 
 interface SliceState {
-  cartItems: string[]
+  cartItems: CartItem[]
   amount: number
   total: number
   isLoading: boolean
 }
 
 const initialState: SliceState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
