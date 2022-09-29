eeb962d11b2f3689ec87a717a7c0069240dba599
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-29 11:40:14 +0530

    feat: setup store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
+import { store } from './components/Store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 
