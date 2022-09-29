97bb5ddab16c7694b6fae150b01ea42db3773c15
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-29 11:41:42 +0530

    refactor: folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
-import { store } from './components/Store'
+import { store } from './Store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
