5892cbc64bf8191b5375b4bb12e8f8fe7b7aaa9e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-29 17:11:57 +0530

    feat: setup folder structure for `CartContainer.tsx` & `CartItem.tsx`

---
+++ src/components/CartContainer.tsx
@@ -0,0 +1,5 @@
+import { CartItem } from './CartItem'
+
+export const CartContainer = () => {
+  return <CartItem />
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { CartContainer } from './CartContainer'
+export { CartItem } from './CartItem'
 export { Navbar } from './Navbar'
