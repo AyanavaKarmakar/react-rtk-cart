b5b79fcfcdcb6effbd0a1bca1826aa060bb2d59d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-30 14:26:17 +0530

    feat: setup `calculateTotal` reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Navbar, CartContainer } from './components'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './Store'
+import { useEffect } from 'react'
+import { calculateTotal } from './features/cart/cartSlice'
 
 export const App = () => {
+  const { cartItems } = useSelector((store: RootState) => store.cart)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotal())
+  }, [cartItems])
+
   return (
     <main>
       <Navbar />
