ff3b77141d16acfd70af99722d9b82c2352b877e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-30 14:55:02 +0530

    feat: implement modal logic

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,28 @@
+import { closeModal } from '../features/modal/modalSlice'
+import { useDispatch } from 'react-redux'
+import { clearCart } from '../features/cart/cartSlice'
+
 export const Modal = () => {
+  const dispatch = useDispatch()
+
+  const handleConfirm = () => {
+    dispatch(clearCart())
+    dispatch(closeModal())
+  }
+
+  const handleCancel = () => {
+    dispatch(closeModal())
+  }
+
   return (
     <aside className='modal-container'>
       <div className='modal'>
         <h4>Remove all items from your shopping cart?</h4>
         <div className='btn-container'>
-          <button type='button' className='btn confirm-btn'>
+          <button type='button' className='btn confirm-btn' onClick={handleConfirm}>
             Confirm
           </button>
-          <button type='button' className='btn clear-btn'>
+          <button type='button' className='btn clear-btn' onClick={handleCancel}>
             Cancel
           </button>
         </div>
