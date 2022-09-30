930b75140dd4ffe44693f9278ef5c875150268d8
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-30 17:19:41 +0530

    feat: add firebase sdk

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcovz4KUrL19nC6QuGWMEuCei_BxZmHqs',
+  authDomain: 'react-rtk-clone.firebaseapp.com',
+  projectId: 'react-rtk-clone',
+  storageBucket: 'react-rtk-clone.appspot.com',
+  messagingSenderId: '584301351907',
+  appId: '1:584301351907:web:c2dc8620e5ab6c633386d6',
+}
+
+const app = initializeApp(firebaseConfig)
